71954133be0540efe15a4bec8780b85a5e3f1688
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-14 01:30:46 +0000

    Created Util class and added "LoadScene" function

---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public static class Util
+{
+    public static void LoadScene(SCENE scene, LoadSceneMode mode = LoadSceneMode.Single)
+    {
+        SceneManager.LoadScene((int)scene, mode);
+    }
+}
