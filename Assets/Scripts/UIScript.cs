8b734fc3d301313f090a75d30a3c2a7100b794fe
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-14 17:45:33 +0000

    Created generic script for handing UI in title and end scenes

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    [SerializeField]
+    Button btnPlay;
+
+    [SerializeField]
+    Button btnQuit;
+
+    void Start()
+    {
+        if (btnPlay != null)
+        {
+            //Set "PlayLevel" to run when "btnPlay" is clicked.
+            btnPlay.onClick.AddListener(() => { StartCoroutine(PlayLevel()); });
+        }
+        else
+        {
+            Debug.LogError("btnPlay hasn't been set.");
+        }
+
+        if (btnQuit != null)
+        {
+            //Set "Quit" to run when "btnQuit" is clicked.
+            btnQuit.onClick.AddListener(() => { StartCoroutine(Quit()); });
+        }
+        else
+        {
+            Debug.LogError("btnQuit hasn't been set.");
+        }
+    }
+
+    IEnumerator PlayLevel()
+    {
+        yield return 0;
+        Util.LoadSceneWithLoading(SCENE.GAME);
+    }
+
+    /// <summary>
+    /// Wait a predetermined length of time before quitting.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Quit()
+    {
+        yield return 0;
+        Util.Quit();
+    }
+}
