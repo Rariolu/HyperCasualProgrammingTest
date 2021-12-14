7e080c51c5cab7c0a90fccde45336492f010d8e6
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-14 18:26:27 +0000

    Made player require a rigidbody to control physics

--- Assets/Scripts/DemoLevel/Player.cs
+++ Assets/Scripts/DemoLevel/Player.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 namespace DemoLevel
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class Player : MonoBehaviour
     {
+        Rigidbody2D body;
+        Rigidbody2D RigidBody
+        {
+            get
+            {
+                if (body == null)
+                {
+                    return body = GetComponent<Rigidbody2D>();
+                }
+                return body;
+            }
+        }
         // Start is called before the first frame update
         void Start()
         {
