b90758e55c52e2fde2c11015411b39d03ec4180d
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-15 19:07:08 +0000

    Added PriorityQueue class for having highest priority nodes at the front

---
+++ Assets/Scripts/DemoLevel/PriorityQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLevel
+{
+    public class PriorityQueue<T> : List<T>
+    {
+        Comparison<T> prioritySort;
+        bool elementAdded = false;
+        bool sortFunctionSet = false;
+        public PriorityQueue()
+        {
+
+        }
+        public PriorityQueue(Comparison<T> prioritysort)
+        {
+            prioritySort = prioritysort;
+            sortFunctionSet = true;
+        }
+        public T Dequeue()
+        {
+            T val = Top();
+            RemoveAt(0);
+            return val;
+        }
+        public void Enqueue(T val)
+        {
+            Add(val);
+            elementAdded = true;
+        }
+        public T Top()
+        {
+            if (elementAdded && sortFunctionSet)
+            {
+                Sort(prioritySort);
+                elementAdded = false;
+            }
+            return this[0];
+        }
+    }
+}
