f7f1ca43c4225769c0d13148e5a1712c440a5d43
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-15 20:26:48 +0000

    Created parent script for things navigating the node map

---
+++ Assets/Scripts/DemoLevel/MapObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DemoLevel
+{
+    public class MapObject : MonoBehaviour
+    {
+        [SerializeField]
+        MapNode startNode;
+
+        MapNode currentNode;
+        public MapNode CurrentMapNode
+        {
+            get
+            {
+                return currentNode;
+            }
+        }
+
+        protected virtual void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.GameObjectIs(TAG.NODE))
+            {
+                MapNode newNode = collision.gameObject.GetComponent<MapNode>();
+                if (newNode != null)
+                {
+                    currentNode = newNode;
+                    Debug.Log("Current node changed");
+                }
+            }
+        }
+
+        protected virtual void Start()
+        {
+            currentNode = startNode;
+        }
+    }
+}
