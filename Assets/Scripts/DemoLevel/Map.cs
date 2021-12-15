95ff131b811cd026a679b5d7e69fc99fd918b162
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-15 19:22:55 +0000

    Implemented A* pathfinding to find optimal path between two nodes.

--- Assets/Scripts/DemoLevel/Map.cs
+++ Assets/Scripts/DemoLevel/Map.cs
@@ -1,16 +1,101 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace DemoLevel
 {
-
+    public delegate void PathDataReset();
     public class Map : MonoBehaviour
     {
+        #region SingletonSetup
+        static Map instance;
+        public static bool InstanceAvailable(out Map map)
+        {
+            map = instance;
+            return map != null;
+        }
+
+        private void Awake()
+        {
+            instance = this;
+        }
+        #endregion
+
+        MapNode nodes;
+        public PathDataReset PathDataReset;
+
+        /// <summary>
+        /// Performs an A* search to find the optimal path between the
+        /// given start and end nodes.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public List<MapNode> GetPath(MapNode start, MapNode end)
+        {
+            PathDataReset();
+            List<MapNode> closedList = new List<MapNode>();
+            Comparison<MapNode> comparison = (n1, n2) =>
+            {
+                return n1.CompareTo(n2);
+            };
+            PriorityQueue<MapNode> openList = new PriorityQueue<MapNode>(comparison);
+            openList.Add(start);
+            while(openList.Count > 0)
+            {
+                MapNode currentNode = openList.Dequeue();
+                closedList.Add(currentNode);
+                if (currentNode == end)
+                {
+                    return GetFoundPath(currentNode);
+                }
+                MapNode[] neighbours = currentNode.Neighbours;
+                foreach(MapNode neighbour in neighbours)
+                {
+                    if (closedList.Contains(neighbour))
+                    {
+                        continue;
+                    }
+                    float cost = Vector2.Distance(currentNode.Position, neighbour.Position);
+                    float g = currentNode.pathData.g + cost;
+                    float h = Vector2.Distance(neighbour.Position, end.Position);
+                    float f = g + h;
+                    if (f < neighbour.pathData.f || !neighbour.pathData.onOpenList)
+                    {
+                        neighbour.pathData.f = f;
+                        neighbour.pathData.g = g;
+                        neighbour.pathData.h = h;
+                        neighbour.pathData.parent = currentNode;
+                        if (!neighbour.pathData.onOpenList)
+                        {
+                            neighbour.pathData.onOpenList = true;
+                            openList.Add(neighbour);
+                        }
+                    }
+                }
+            }
+            return GetFoundPath(null);
+        }
+
+        List<MapNode> GetFoundPath(MapNode end)
+        {
+            List<MapNode> foundPath = new List<MapNode>();
+            while (end != null)
+            {
+                foundPath.Add(end);
+                end = end.pathData.parent;
+            }
+            foundPath.Reverse();
+            return foundPath;
+        }
+
+        
+
         // Start is called before the first frame update
         void Start()
         {
-
+            nodes = GetComponentInChildren<MapNode>();
         }
 
         // Update is called once per frame
@@ -19,5 +104,4 @@ namespace DemoLevel
 
         }
     }
-
 }
--- Assets/Scripts/DemoLevel/MapNode.cs
+++ Assets/Scripts/DemoLevel/MapNode.cs
@@ -6,16 +6,67 @@ namespace DemoLevel
 {
     public class MapNode : MonoBehaviour
     {
+        public Vector2 Position
+        {
+            get
+            {
+                return new Vector2(transform.position.x, transform.position.y);
+            }
+        }
+
+        [SerializeField]
+        MapNode[] neighbours;
+
+        public MapNode[] Neighbours
+        {
+            get
+            {
+                return neighbours;
+            }
+        }
+
+        public MapPathfindingNode pathData;
+
+        public int CompareTo(MapNode node)
+        {
+            if (pathData.f < node.pathData.f)
+            {
+                return -1;
+            }
+            else if (pathData.f > node.pathData.f)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        void ResetPathData()
+        {
+            pathData = new MapPathfindingNode();
+        }
+
         // Start is called before the first frame update
         void Start()
         {
-
+            Map map;
+            if (Map.InstanceAvailable(out map))
+            {
+                map.PathDataReset += ResetPathData;
+            }
         }
+    }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+    /// <summary>
+    /// A structure used within the pathfinding algorithm to store
+    /// extraneous data that is only relevant in that scenario.
+    /// </summary>
+    public struct MapPathfindingNode
+    {
+        public float g;
+        public float h;
+        public float f;
+        public bool onOpenList;
+        public MapNode parent;
     }
 }
