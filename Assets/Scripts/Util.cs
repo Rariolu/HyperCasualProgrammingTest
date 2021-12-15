05e9cd65314c3424001bd88583661e01fe1a08a9
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-15 13:50:49 +0000

    Added "NormaliseString" and "GameObjectIs" functions

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -3,8 +3,22 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEditor;
 
+/// <summary>
+/// A static class full of generally useful utility functions.
+/// </summary>
 public static class Util
 {
+    /// <summary>
+    /// Check if the gameobject has the given tag.
+    /// </summary>
+    /// <param name="gameObject"></param>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    public static bool GameObjectIs(this GameObject gameObject, TAG tag)
+    {
+        return gameObject.tag.NormaliseString() == tag.NormaliseString();
+    }
+
     /// <summary>
     /// Show the "Loading" scene as a buffer while the desired scene loads.
     /// </summary>
@@ -23,6 +37,16 @@ public static class Util
     public static void LoadScene(SCENE scene, LoadSceneMode mode = LoadSceneMode.Single)
     {
         SceneManager.LoadScene((int)scene, mode);
+    }
+
+    /// <summary>
+    /// Converts the given object into a string and "normalises" it (i.e. trims trailing whitespace and makes it lowercase).
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    public static string NormaliseString(this object obj)
+    {
+        return obj.ToString().Trim().ToLower();
     }
 
     /// <summary>
