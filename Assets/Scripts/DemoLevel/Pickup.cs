3ffc923cfb960aacb416fa5c2d3c475d6e44c061
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-17 20:58:07 +0000

    Implemented pickup type enum

--- Assets/Scripts/DemoLevel/Pickup.cs
+++ Assets/Scripts/DemoLevel/Pickup.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pickup : MonoBehaviour
+namespace DemoLevel
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Pickup : MonoBehaviour
     {
-        
-    }
+        public enum PICKUP_ITEM
+        {
+            COIN
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField]
+        PICKUP_ITEM type = PICKUP_ITEM.COIN;
+
+        public PICKUP_ITEM ItemType
+        {
+            get
+            {
+                return type;
+            }
+        }
+
+        public void PickedUp()
+        {
+            Destroy(gameObject);
+        }
     }
 }
