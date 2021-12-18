6b2627ab818ce5bc139c2c28e3d9b9cb91dd0a60
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-18 02:38:54 +0000

    Created "GameStats" class to contain stats such as the end state.

---
+++ Assets/Scripts/GameStats.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStats
+{
+    #region SingletonSetup
+    static GameStats instance;
+    public static GameStats Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                return instance = new GameStats();
+            }
+            return instance;
+        }
+    }
+
+    private GameStats()
+    {
+
+    }
+
+    public static void Reset()
+    {
+        instance = null;
+    }
+    #endregion
+
+    END_STATE endState;
+    public END_STATE EndState
+    {
+        get
+        {
+            return endState;
+        }
+        set
+        {
+            endState = value;
+
+            Util.LoadSceneWithLoading(SCENE.END);
+        }
+    }
+}
