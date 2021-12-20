9773265e71522ad03e36c22b524912f3e4f54334
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-20 15:22:56 +0000

    Setting default speed bursts

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -98,5 +98,7 @@ public class UIManager : MonoBehaviour
                 btnBurst.onClick.AddListener(() => { player.UseBurst(); });
             }
         }
+
+        SetSpeedBursts(0);
     }
 }
