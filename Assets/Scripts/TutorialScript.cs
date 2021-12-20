81a9095e429fb66998681ad4652118ab695a0217
Coyne, Sophie <rcoyneyr82011@gmail.com>
2021-12-20 20:45:35 +0000

    Created script to manage tutorials

---
+++ Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialScript : MonoBehaviour
+{
+    static bool tutorialHasPlayed = false;
+
+    [SerializeField]
+    Button btnNext;
+
+    [SerializeField]
+    GameObject levelRoot;
+
+    [SerializeField]
+    GameObject[] tutorialPanels;
+
+    int panelIndex = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!tutorialHasPlayed)
+        {
+            btnNext.onClick.AddListener(BtnNextClick);
+            BtnNextClick();
+        }
+        else
+        {
+            levelRoot.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+
+    void BtnNextClick()
+    {
+        StaticSoundManager.PlaySoundAsync(SOUND.BUTTON_CLICK);
+        if (panelIndex > -1)
+        {
+            tutorialPanels[panelIndex].SetActive(false);
+        }
+
+        panelIndex++;
+        if (panelIndex < tutorialPanels.Length)
+        {
+            tutorialPanels[panelIndex].SetActive(true);
+        }
+        else
+        {
+            tutorialHasPlayed = true;
+            levelRoot.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
